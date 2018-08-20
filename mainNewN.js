629a7bc9f0f3b80e5132e3f21a00ce8b6c0c297f
XS金玉树 <245659401@qq.com>
2018-08-20 17:40:08 +0800

    Update mainNewN.js

--- mainNewN.js
+++ mainNewN.js
@@ -1,12 +1,10 @@
-require("UIColor");
+require("HomeViewController, LotteryViewController, MoneyViewController, MyViewController");
 
-defineClass("FirstViewController", {
-    viewDidLoad: function() {
-        self.super().viewDidLoad();
-        self.view().setBackgroundColor(UIColor.greenColor());
-    },
-    viewWillAppear: function(animated) {
-        self.super().viewWillAppear(animated);
-        NSLog("OK了");
+defineClass("JCTabBarController", {
+    addChildViewControllers: function() {
+        self.addChildViewController_title_imageName_selectImageName(HomeViewController.new(), "666", "TabBar_home_23x23_", "TabBar_home_23x23_selected");
+        self.addChildViewController_title_imageName_selectImageName(LotteryViewController.new(), "666", "TabBar_win_23x23_", "TabBar_win_23x23_selected");
+        self.addChildViewController_title_imageName_selectImageName(MoneyViewController.new(), "资金明细", "TabBar_money_23x23_", "TabBar_money_23x23_selected");
+        self.addChildViewController_title_imageName_selectImageName(MyViewController.new(), "我的", "TabBar_my_23x23_", "TabBar_my_23x23_selected");
     }
 }, {});
